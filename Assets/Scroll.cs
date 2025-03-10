14b2309ecd7c57d396de261fa116b4accbe697f3
KazThomas <mr.firebass@gmail.com>
2025-03-10 17:35:38 +0000

    Icons + Scrollbar
    Icon functionality with every body part and fully working scroll bar

---
+++ Assets/Scroll.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scroll : MonoBehaviour
+{
+    [SerializeField] private GameObject icons;
+    private Scrollbar roll;
+    private float rollPos;
+    [SerializeField] private float yPos = 3f;
+    private void Start()
+    {
+        roll = GetComponent<Scrollbar>();
+        rollPos = roll.value;
+    }
+    public void OnMouseDrag()
+    {
+        Debug.Log("test");
+
+        if (roll.value > rollPos)
+        {
+           Transform rect = icons.GetComponent<RectTransform>();
+            rect.GetComponent<RectTransform>().localPosition += new Vector3(0, yPos, 0);
+            rollPos = roll.value;
+        }
+
+        if (roll.value < rollPos)
+        {
+            Transform rect = icons.GetComponent<RectTransform>();
+            rect.GetComponent<RectTransform>().localPosition -= new Vector3(0, yPos, 0);
+            rollPos = roll.value;
+        }
+
+    }
+}
